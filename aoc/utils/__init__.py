15b22ce72b39111b593814030e943f95c8cd2c9e
Pedro Piquero <gowikel@gmail.com>
2024-04-05 21:32:45 +0100

    Clean up file

--- aoc/utils/__init__.py
+++ aoc/utils/__init__.py
@@ -1,13 +1,8 @@
 import os
-from functools import lru_cache
 
 from dotenv import dotenv_values
-from rich.console import Console
-
-console = Console()
 
 
-@lru_cache(maxsize=None)
 def load_config():
     return {**dotenv_values(), **os.environ}
 
